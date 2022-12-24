53e2e474872368e6d20d9b4f560ef1771765701b
Austin Marshall <75810734+microbemarsh@users.noreply.github.com>
2022-12-24 15:26:08 -0500

    Add files via upload
    After guppy basecalling and before 16S relative abundance visualization

---
+++ SAME22_fq_concat.bash
@@ -0,0 +1,160 @@
+#! /bin/bash
+
+conda activate seqkit
+
+seqkit scat -j 8 -f /home/marshaag/SAME2022/22_10_07_SLR_RUN5/pass/barcode01 > /home/marshaag/SAME2022/scat_reads/SLR1.fq
+seqkit scat -j 8 -f /home/marshaag/SAME2022/22_09_01_SLR_RUN2/pass/barcode17 > /home/marshaag/SAME2022/scat_reads/SLR2.fq
+seqkit scat -j 8 -f /home/marshaag/SAME2022/22_08_26_SLR_RUN1/pass/barcode18 > /home/marshaag/SAME2022/scat_reads/SLR3.fq
+seqkit scat -j 8 -f /home/marshaag/SAME2022/22_08_26_SLR_RUN1/pass/barcode19 > /home/marshaag/SAME2022/scat_reads/SLR4.fq
+seqkit scat -j 8 -f /home/marshaag/SAME2022/22_10_12_SLR_RUN6/pass/barcode09 > /home/marshaag/SAME2022/scat_reads/SLR5.fq
+seqkit scat -j 8 -f /home/marshaag/SAME2022/22_08_26_SLR_RUN1/pass/barcode20 > /home/marshaag/SAME2022/scat_reads/SLR6.fq
+seqkit scat -j 8 -f /home/marshaag/SAME2022/22_10_07_SLR_RUN5/pass/barcode17 > /home/marshaag/SAME2022/scat_reads/SLR7.fq
+seqkit scat -j 8 -f /home/marshaag/SAME2022/22_10_07_SLR_RUN5/pass/barcode18 > /home/marshaag/SAME2022/scat_reads/SLR8.fq
+seqkit scat -j 8 -f /home/marshaag/SAME2022/22_11_16_SLR_RUN8/pass/barcode11 > /home/marshaag/SAME2022/scat_reads/SLR9.fq
+seqkit scat -j 8 -f /home/marshaag/SAME2022/22_11_02_SLR_RUN7/pass/barcode01 > /home/marshaag/SAME2022/scat_reads/SLR10.fq
+seqkit scat -j 8 -f /home/marshaag/SAME2022/22_11_16_SLR_RUN8/pass/barcode12 > /home/marshaag/SAME2022/scat_reads/SLR11.fq
+seqkit scat -j 8 -f /home/marshaag/SAME2022/22_11_16_SLR_RUN8/pass/barcode13 > /home/marshaag/SAME2022/scat_reads/SLR12.fq
+seqkit scat -j 8 -f /home/marshaag/SAME2022/22_11_02_SLR_RUN7/pass/barcode02 > /home/marshaag/SAME2022/scat_reads/SLR13.fq
+seqkit scat -j 8 -f /home/marshaag/SAME2022/22_08_26_SLR_RUN1/pass/barcode14 > /home/marshaag/SAME2022/scat_reads/SLR14.fq
+seqkit scat -j 8 -f /home/marshaag/SAME2022/22_08_26_SLR_RUN1/pass/barcode15 > /home/marshaag/SAME2022/scat_reads/SLR15.fq
+seqkit scat -j 8 -f /home/marshaag/SAME2022/22_08_26_SLR_RUN1/pass/barcode16 > /home/marshaag/SAME2022/scat_reads/SLR16.fq
+seqkit scat -j 8 -f /home/marshaag/SAME2022/22_09_01_SLR_RUN2/pass/barcode09 > /home/marshaag/SAME2022/scat_reads/SLR17.fq
+seqkit scat -j 8 -f /home/marshaag/SAME2022/22_09_01_SLR_RUN2/pass/barcode10 > /home/marshaag/SAME2022/scat_reads/SLR18.fq
+seqkit scat -j 8 -f /home/marshaag/SAME2022/22_09_01_SLR_RUN2/pass/barcode11 > /home/marshaag/SAME2022/scat_reads/SLR19.fq
+seqkit scat -j 8 -f /home/marshaag/SAME2022/22_11_02_SLR_RUN7/pass/barcode03 > /home/marshaag/SAME2022/scat_reads/SLR20.fq
+seqkit scat -j 8 -f /home/marshaag/SAME2022/22_09_22_SLR_RUN4/pass/barcode19 > /home/marshaag/SAME2022/scat_reads/SLR21.fq
+seqkit scat -j 8 -f /home/marshaag/SAME2022/22_09_01_SLR_RUN2/pass/barcode12 > /home/marshaag/SAME2022/scat_reads/SLR22.fq
+seqkit scat -j 8 -f /home/marshaag/SAME2022/22_09_22_SLR_RUN4/pass/barcode20 > /home/marshaag/SAME2022/scat_reads/SLR23.fq
+seqkit scat -j 8 -f /home/marshaag/SAME2022/22_11_02_SLR_RUN7/pass/barcode04 > /home/marshaag/SAME2022/scat_reads/SLR24.fq
+seqkit scat -j 8 -f /home/marshaag/SAME2022/22_09_01_SLR_RUN2/pass/barcode13 > /home/marshaag/SAME2022/scat_reads/SLR25.fq
+seqkit scat -j 8 -f /home/marshaag/SAME2022/22_09_01_SLR_RUN2/pass/barcode14 > /home/marshaag/SAME2022/scat_reads/SLR26.fq
+seqkit scat -j 8 -f /home/marshaag/SAME2022/22_09_01_SLR_RUN2/pass/barcode15 > /home/marshaag/SAME2022/scat_reads/SLR27.fq
+seqkit scat -j 8 -f /home/marshaag/SAME2022/22_10_07_SLR_RUN5/pass/barcode02 > /home/marshaag/SAME2022/scat_reads/SLR28.fq
+seqkit scat -j 8 -f /home/marshaag/SAME2022/22_09_01_SLR_RUN2/pass/barcode16 > /home/marshaag/SAME2022/scat_reads/SLR29.fq
+seqkit scat -j 8 -f /home/marshaag/SAME2022/22_11_02_SLR_RUN7/pass/barcode15 > /home/marshaag/SAME2022/scat_reads/SLR30.fq
+seqkit scat -j 8 -f /home/marshaag/SAME2022/22_10_07_SLR_RUN5/pass/barcode19 > /home/marshaag/SAME2022/scat_reads/SLR31.fq
+seqkit scat -j 8 -f /home/marshaag/SAME2022/22_10_07_SLR_RUN5/pass/barcode03 > /home/marshaag/SAME2022/scat_reads/SLR32.fq
+seqkit scat -j 8 -f /home/marshaag/SAME2022/22_10_07_SLR_RUN5/pass/barcode04 > /home/marshaag/SAME2022/scat_reads/SLR33.fq
+seqkit scat -j 8 -f /home/marshaag/SAME2022/22_09_22_SLR_RUN4/pass/barcode10 > /home/marshaag/SAME2022/scat_reads/SLR34.fq
+seqkit scat -j 8 -f /home/marshaag/SAME2022/22_09_12_SLR_RUN3/pass/barcode01 > /home/marshaag/SAME2022/scat_reads/SLR35.fq
+seqkit scat -j 8 -f /home/marshaag/SAME2022/22_09_22_SLR_RUN4/pass/barcode21 > /home/marshaag/SAME2022/scat_reads/SLR36.fq
+seqkit scat -j 8 -f /home/marshaag/SAME2022/22_11_02_SLR_RUN7/pass/barcode05 > /home/marshaag/SAME2022/scat_reads/SLR37.fq
+seqkit scat -j 8 -f /home/marshaag/SAME2022/22_10_07_SLR_RUN5/pass/barcode20 > /home/marshaag/SAME2022/scat_reads/SLR38.fq
+seqkit scat -j 8 -f /home/marshaag/SAME2022/22_11_02_SLR_RUN7/pass/barcode06 > /home/marshaag/SAME2022/scat_reads/SLR39.fq
+seqkit scat -j 8 -f /home/marshaag/SAME2022/22_10_12_SLR_RUN6/pass/barcode21 > /home/marshaag/SAME2022/scat_reads/SLR40.fq
+seqkit scat -j 8 -f /home/marshaag/SAME2022/22_09_12_SLR_RUN3/pass/barcode03 > /home/marshaag/SAME2022/scat_reads/SLR41.fq
+seqkit scat -j 8 -f /home/marshaag/SAME2022/22_09_22_SLR_RUN4/pass/barcode11 > /home/marshaag/SAME2022/scat_reads/SLR42.fq
+seqkit scat -j 8 -f /home/marshaag/SAME2022/22_09_12_SLR_RUN3/pass/barcode04 > /home/marshaag/SAME2022/scat_reads/SLR43.fq
+seqkit scat -j 8 -f /home/marshaag/SAME2022/22_09_12_SLR_RUN3/pass/barcode05 > /home/marshaag/SAME2022/scat_reads/SLR44.fq
+seqkit scat -j 8 -f /home/marshaag/SAME2022/22_09_12_SLR_RUN3/pass/barcode06 > /home/marshaag/SAME2022/scat_reads/SLR45.fq
+seqkit scat -j 8 -f /home/marshaag/SAME2022/22_09_22_SLR_RUN4/pass/barcode22 > /home/marshaag/SAME2022/scat_reads/SLR46.fq
+seqkit scat -j 8 -f /home/marshaag/SAME2022/22_09_12_SLR_RUN3/pass/barcode08 > /home/marshaag/SAME2022/scat_reads/SLR47.fq
+seqkit scat -j 8 -f /home/marshaag/SAME2022/22_09_12_SLR_RUN3/pass/barcode17 > /home/marshaag/SAME2022/scat_reads/SLR48.fq
+seqkit scat -j 8 -f /home/marshaag/SAME2022/22_10_12_SLR_RUN6/pass/barcode22 > /home/marshaag/SAME2022/scat_reads/SLR49.fq
+seqkit scat -j 8 -f /home/marshaag/SAME2022/22_09_12_SLR_RUN3/pass/barcode18 > /home/marshaag/SAME2022/scat_reads/SLR50.fq
+seqkit scat -j 8 -f /home/marshaag/SAME2022/22_10_07_SLR_RUN5/pass/barcode05 > /home/marshaag/SAME2022/scat_reads/SLR51.fq
+seqkit scat -j 8 -f /home/marshaag/SAME2022/22_09_12_SLR_RUN3/pass/barcode19 > /home/marshaag/SAME2022/scat_reads/SLR52.fq
+seqkit scat -j 8 -f /home/marshaag/SAME2022/22_09_12_SLR_RUN3/pass/barcode20 > /home/marshaag/SAME2022/scat_reads/SLR53.fq
+seqkit scat -j 8 -f /home/marshaag/SAME2022/22_09_12_SLR_RUN3/pass/barcode21 > /home/marshaag/SAME2022/scat_reads/SLR55.fq
+seqkit scat -j 8 -f /home/marshaag/SAME2022/22_09_12_SLR_RUN3/pass/barcode22 > /home/marshaag/SAME2022/scat_reads/SLR56.fq
+seqkit scat -j 8 -f /home/marshaag/SAME2022/22_10_12_SLR_RUN6/pass/barcode23 > /home/marshaag/SAME2022/scat_reads/SLR57.fq
+seqkit scat -j 8 -f /home/marshaag/SAME2022/22_10_12_SLR_RUN6/pass/barcode24 > /home/marshaag/SAME2022/scat_reads/SLR58.fq
+seqkit scat -j 8 -f /home/marshaag/SAME2022/22_11_02_SLR_RUN7/pass/barcode07 > /home/marshaag/SAME2022/scat_reads/SLR59.fq
+seqkit scat -j 8 -f /home/marshaag/SAME2022/22_10_07_SLR_RUN5/pass/barcode06 > /home/marshaag/SAME2022/scat_reads/SLR60.fq
+seqkit scat -j 8 -f /home/marshaag/SAME2022/22_11_02_SLR_RUN7/pass/barcode16 > /home/marshaag/SAME2022/scat_reads/SLR61.fq
+seqkit scat -j 8 -f /home/marshaag/SAME2022/22_10_07_SLR_RUN5/pass/barcode07 > /home/marshaag/SAME2022/scat_reads/SLR62.fq
+seqkit scat -j 8 -f /home/marshaag/SAME2022/22_11_02_SLR_RUN7/pass/barcode08 > /home/marshaag/SAME2022/scat_reads/SLR63.fq
+seqkit scat -j 8 -f /home/marshaag/SAME2022/22_09_22_SLR_RUN4/pass/barcode23 > /home/marshaag/SAME2022/scat_reads/SLR66.fq
+seqkit scat -j 8 -f /home/marshaag/SAME2022/22_09_22_SLR_RUN4/pass/barcode24 > /home/marshaag/SAME2022/scat_reads/SLR67.fq
+seqkit scat -j 8 -f /home/marshaag/SAME2022/22_08_26_SLR_RUN1/pass/barcode23 > /home/marshaag/SAME2022/scat_reads/SLR68.fq
+seqkit scat -j 8 -f /home/marshaag/SAME2022/22_08_26_SLR_RUN1/pass/barcode24 > /home/marshaag/SAME2022/scat_reads/SLR69.fq
+seqkit scat -j 8 -f /home/marshaag/SAME2022/22_10_07_SLR_RUN5/pass/barcode08 > /home/marshaag/SAME2022/scat_reads/SLR70.fq
+seqkit scat -j 8 -f /home/marshaag/SAME2022/22_09_22_SLR_RUN4/pass/barcode15 > /home/marshaag/SAME2022/scat_reads/SLR71.fq
+seqkit scat -j 8 -f /home/marshaag/SAME2022/22_08_26_SLR_RUN1/pass/barcode02 > /home/marshaag/SAME2022/scat_reads/SLR72.fq
+seqkit scat -j 8 -f /home/marshaag/SAME2022/22_09_01_SLR_RUN2/pass/barcode03 > /home/marshaag/SAME2022/scat_reads/SLR73.fq
+
+seqkit stat /home/marshaag/SAME2022/scat_reads/*.fq -a -j 20 -E sanger -T | csvtk csv2md -t
+
+conda deactivate
+
+cd /home/marshaag/SAME2022/scat_reads
+
+conda activate happy_emu
+
+emu abundance SLR1.fq --db /home/marshaag/emu_gitlab/emu_database --threads 20 --keep-counts --output-dir emu_results
+emu abundance SLR2.fq --db /home/marshaag/emu_gitlab/emu_database --threads 20 --keep-counts --output-dir emu_results
+emu abundance SLR3.fq --db /home/marshaag/emu_gitlab/emu_database --threads 20 --keep-counts --output-dir emu_results
+emu abundance SLR4.fq --db /home/marshaag/emu_gitlab/emu_database --threads 20 --keep-counts --output-dir emu_results
+emu abundance SLR5.fq --db /home/marshaag/emu_gitlab/emu_database --threads 20 --keep-counts --output-dir emu_results
+emu abundance SLR6.fq --db /home/marshaag/emu_gitlab/emu_database --threads 20 --keep-counts --output-dir emu_results
+emu abundance SLR7.fq --db /home/marshaag/emu_gitlab/emu_database --threads 20 --keep-counts --output-dir emu_results
+emu abundance SLR8.fq --db /home/marshaag/emu_gitlab/emu_database --threads 20 --keep-counts --output-dir emu_results
+emu abundance SLR9.fq --db /home/marshaag/emu_gitlab/emu_database --threads 20 --keep-counts --output-dir emu_results
+emu abundance SLR10.fq --db /home/marshaag/emu_gitlab/emu_database --threads 20 --keep-counts --output-dir emu_results
+emu abundance SLR11.fq --db /home/marshaag/emu_gitlab/emu_database --threads 20 --keep-counts --output-dir emu_results
+emu abundance SLR12.fq --db /home/marshaag/emu_gitlab/emu_database --threads 20 --keep-counts --output-dir emu_results
+emu abundance SLR13.fq --db /home/marshaag/emu_gitlab/emu_database --threads 20 --keep-counts --output-dir emu_results
+emu abundance SLR14.fq --db /home/marshaag/emu_gitlab/emu_database --threads 20 --keep-counts --output-dir emu_results
+emu abundance SLR15.fq --db /home/marshaag/emu_gitlab/emu_database --threads 20 --keep-counts --output-dir emu_results
+emu abundance SLR16.fq --db /home/marshaag/emu_gitlab/emu_database --threads 20 --keep-counts --output-dir emu_results
+emu abundance SLR17.fq --db /home/marshaag/emu_gitlab/emu_database --threads 20 --keep-counts --output-dir emu_results
+emu abundance SLR18.fq --db /home/marshaag/emu_gitlab/emu_database --threads 20 --keep-counts --output-dir emu_results
+emu abundance SLR19.fq --db /home/marshaag/emu_gitlab/emu_database --threads 20 --keep-counts --output-dir emu_results
+emu abundance SLR20.fq --db /home/marshaag/emu_gitlab/emu_database --threads 20 --keep-counts --output-dir emu_results
+emu abundance SLR21.fq --db /home/marshaag/emu_gitlab/emu_database --threads 20 --keep-counts --output-dir emu_results
+emu abundance SLR22.fq --db /home/marshaag/emu_gitlab/emu_database --threads 20 --keep-counts --output-dir emu_results
+emu abundance SLR23.fq --db /home/marshaag/emu_gitlab/emu_database --threads 20 --keep-counts --output-dir emu_results
+emu abundance SLR24.fq --db /home/marshaag/emu_gitlab/emu_database --threads 20 --keep-counts --output-dir emu_results
+emu abundance SLR25.fq --db /home/marshaag/emu_gitlab/emu_database --threads 20 --keep-counts --output-dir emu_results
+emu abundance SLR26.fq --db /home/marshaag/emu_gitlab/emu_database --threads 20 --keep-counts --output-dir emu_results
+emu abundance SLR27.fq --db /home/marshaag/emu_gitlab/emu_database --threads 20 --keep-counts --output-dir emu_results
+emu abundance SLR28.fq --db /home/marshaag/emu_gitlab/emu_database --threads 20 --keep-counts --output-dir emu_results
+emu abundance SLR29.fq --db /home/marshaag/emu_gitlab/emu_database --threads 20 --keep-counts --output-dir emu_results
+emu abundance SLR30.fq --db /home/marshaag/emu_gitlab/emu_database --threads 20 --keep-counts --output-dir emu_results
+emu abundance SLR31.fq --db /home/marshaag/emu_gitlab/emu_database --threads 20 --keep-counts --output-dir emu_results
+emu abundance SLR32.fq --db /home/marshaag/emu_gitlab/emu_database --threads 20 --keep-counts --output-dir emu_results
+emu abundance SLR33.fq --db /home/marshaag/emu_gitlab/emu_database --threads 20 --keep-counts --output-dir emu_results
+emu abundance SLR34.fq --db /home/marshaag/emu_gitlab/emu_database --threads 20 --keep-counts --output-dir emu_results
+emu abundance SLR35.fq --db /home/marshaag/emu_gitlab/emu_database --threads 20 --keep-counts --output-dir emu_results
+emu abundance SLR36.fq --db /home/marshaag/emu_gitlab/emu_database --threads 20 --keep-counts --output-dir emu_results
+emu abundance SLR37.fq --db /home/marshaag/emu_gitlab/emu_database --threads 20 --keep-counts --output-dir emu_results
+emu abundance SLR38.fq --db /home/marshaag/emu_gitlab/emu_database --threads 20 --keep-counts --output-dir emu_results
+emu abundance SLR39.fq --db /home/marshaag/emu_gitlab/emu_database --threads 20 --keep-counts --output-dir emu_results
+emu abundance SLR40.fq --db /home/marshaag/emu_gitlab/emu_database --threads 20 --keep-counts --output-dir emu_results
+emu abundance SLR41.fq --db /home/marshaag/emu_gitlab/emu_database --threads 20 --keep-counts --output-dir emu_results
+emu abundance SLR42.fq --db /home/marshaag/emu_gitlab/emu_database --threads 20 --keep-counts --output-dir emu_results
+emu abundance SLR43.fq --db /home/marshaag/emu_gitlab/emu_database --threads 20 --keep-counts --output-dir emu_results
+emu abundance SLR44.fq --db /home/marshaag/emu_gitlab/emu_database --threads 20 --keep-counts --output-dir emu_results
+emu abundance SLR45.fq --db /home/marshaag/emu_gitlab/emu_database --threads 20 --keep-counts --output-dir emu_results
+emu abundance SLR46.fq --db /home/marshaag/emu_gitlab/emu_database --threads 20 --keep-counts --output-dir emu_results
+emu abundance SLR47.fq --db /home/marshaag/emu_gitlab/emu_database --threads 20 --keep-counts --output-dir emu_results
+emu abundance SLR48.fq --db /home/marshaag/emu_gitlab/emu_database --threads 20 --keep-counts --output-dir emu_results
+emu abundance SLR49.fq --db /home/marshaag/emu_gitlab/emu_database --threads 20 --keep-counts --output-dir emu_results
+emu abundance SLR50.fq --db /home/marshaag/emu_gitlab/emu_database --threads 20 --keep-counts --output-dir emu_results
+emu abundance SLR51.fq --db /home/marshaag/emu_gitlab/emu_database --threads 20 --keep-counts --output-dir emu_results
+emu abundance SLR52.fq --db /home/marshaag/emu_gitlab/emu_database --threads 20 --keep-counts --output-dir emu_results
+emu abundance SLR53.fq --db /home/marshaag/emu_gitlab/emu_database --threads 20 --keep-counts --output-dir emu_results
+emu abundance SLR55.fq --db /home/marshaag/emu_gitlab/emu_database --threads 20 --keep-counts --output-dir emu_results
+emu abundance SLR56.fq --db /home/marshaag/emu_gitlab/emu_database --threads 20 --keep-counts --output-dir emu_results
+emu abundance SLR57.fq --db /home/marshaag/emu_gitlab/emu_database --threads 20 --keep-counts --output-dir emu_results
+emu abundance SLR58.fq --db /home/marshaag/emu_gitlab/emu_database --threads 20 --keep-counts --output-dir emu_results
+emu abundance SLR59.fq --db /home/marshaag/emu_gitlab/emu_database --threads 20 --keep-counts --output-dir emu_results
+emu abundance SLR60.fq --db /home/marshaag/emu_gitlab/emu_database --threads 20 --keep-counts --output-dir emu_results
+emu abundance SLR61.fq --db /home/marshaag/emu_gitlab/emu_database --threads 20 --keep-counts --output-dir emu_results
+emu abundance SLR62.fq --db /home/marshaag/emu_gitlab/emu_database --threads 20 --keep-counts --output-dir emu_results
+emu abundance SLR63.fq --db /home/marshaag/emu_gitlab/emu_database --threads 20 --keep-counts --output-dir emu_results
+emu abundance SLR66.fq --db /home/marshaag/emu_gitlab/emu_database --threads 20 --keep-counts --output-dir emu_results
+emu abundance SLR67.fq --db /home/marshaag/emu_gitlab/emu_database --threads 20 --keep-counts --output-dir emu_results
+emu abundance SLR68.fq --db /home/marshaag/emu_gitlab/emu_database --threads 20 --keep-counts --output-dir emu_results
+emu abundance SLR69.fq --db /home/marshaag/emu_gitlab/emu_database --threads 20 --keep-counts --output-dir emu_results
+emu abundance SLR70.fq --db /home/marshaag/emu_gitlab/emu_database --threads 20 --keep-counts --output-dir emu_results
+emu abundance SLR71.fq --db /home/marshaag/emu_gitlab/emu_database --threads 20 --keep-counts --output-dir emu_results
+emu abundance SLR72.fq --db /home/marshaag/emu_gitlab/emu_database --threads 20 --keep-counts --output-dir emu_results
+emu abundance SLR73.fq --db /home/marshaag/emu_gitlab/emu_database --threads 20 --keep-counts --output-dir emu_results
+
+emu combine-outputs --split-tables /home/marshaag/SAME2022/scat_reads/emu_results species
+emu combine-outputs --split-tables /home/marshaag/SAME2022/scat_reads/emu_results genus
+emu combine-outputs --split-tables /home/marshaag/SAME2022/scat_reads/emu_results family
+emu combine-outputs --split-tables /home/marshaag/SAME2022/scat_reads/emu_results order
+emu combine-outputs --split-tables /home/marshaag/SAME2022/scat_reads/emu_results class
+emu combine-outputs --split-tables /home/marshaag/SAME2022/scat_reads/emu_results phylum
